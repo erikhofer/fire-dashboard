32bc1f4a49a4e7c4c927fc6f61b2569a9ddd33d7
Erik Hofer <erik-hofer@mail.de>
2020-08-21 23:41:38 +0200

    Add redux store provider to test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import App from './App'
+import { Provider } from 'react-redux'
+import { Store, createStore } from 'redux'
+import { AppState } from './store/model'
+import { AppAction, reducer } from './store/reducer'
+
+const store: Store<AppState, AppAction> = createStore(reducer)
 
 test('app renders', () => {
-  render(<App />)
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
 })
