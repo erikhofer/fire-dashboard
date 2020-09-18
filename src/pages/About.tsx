8b2338bf3ce7eb9a5f16450aa5023489dae64799
Erik Hofer <erik-hofer@mail.de>
2020-09-19 01:10:34 +0200

    Add info to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { PageContent } from '../components/PageContent'
 import { PageHeader } from '../components/PageHeader'
 import Table, { ColumnsType } from 'antd/lib/table'
+import { Button } from 'antd'
+import { BugOutlined } from '@ant-design/icons'
 
 const columns: ColumnsType = [
   {
@@ -29,8 +31,44 @@ export const About: React.FC = () => {
 
   return (
     <>
-      <PageHeader title="Settings" />
+      <PageHeader title="About" />
       <PageContent>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: 16
+          }}
+        >
+          <img
+            alt="Logo"
+            src={`${process.env.PUBLIC_URL}/logo512.png`}
+            style={{ width: 300, marginRight: 32 }}
+          />
+          <div>
+            <h2>FIRE Dashboard</h2>
+            <p>
+              Version: 0.0.0
+              <br />
+              License:{' '}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/erikhofer/fire-dashboard/blob/master/LICENSE"
+              >
+                AGPL-3.0
+              </a>
+            </p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/erikhofer/fire-dashboard/issues"
+            >
+              <Button icon={<BugOutlined />}>Report Issue</Button>
+            </a>
+          </div>
+        </div>
         <Table
           title={() => 'Third Party Licenses'}
           rowKey="package"
