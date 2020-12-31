b550f44741cd9f7b2300a2c12220c6ba2d36b933
Erik Hofer <erik-hofer@mail.de>
2021-01-01 00:40:54 +0100

    Upgrade to CRA 4

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import 'liquid-loading'
 import { Store, createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
@@ -13,6 +13,7 @@ import { initalState } from './store/dev'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import { PersistGate } from 'redux-persist/integration/react'
+import reportWebVitals from './reportWebVitals'
 
 const persistConfig = {
   key: 'root',
@@ -42,5 +43,10 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister()
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
