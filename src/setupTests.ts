52dea653626d72cfa110abdd56b2aed221965128
Erik Hofer <erik-hofer@mail.de>
2021-01-01 01:01:57 +0100

    Fix tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,17 +4,12 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 
-// fix for https://github.com/ant-design/ant-design/issues/21096
 Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // deprecated
-    removeListener: jest.fn(), // deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn()
-  }))
+  value: () => {
+    return {
+      matches: false,
+      addListener: () => {},
+      removeListener: () => {}
+    }
+  }
 })
